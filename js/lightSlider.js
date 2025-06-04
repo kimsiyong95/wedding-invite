12d92cd93dc078ba30cbdd81e26a90d0184a46fe
a1101911 <kimsiyong95@11stcorp.com>
2025-06-04 10:39:53 +0900

    test

---
+++ js/lightSlider.js
@@ -0,0 +1,1 @@
+console.log('lightSlider loaded');
---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+  $('#imageGallery').lightSlider({
+    gallery:true,
+    item:1,
+    loop:true,
+    thumbItem:3,
+    slideMargin:0,
+    enableDrag: true,
+    currentPagerPosition:'left',
+    auto: true
+  });
+});
